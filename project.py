33a7e4e8e134c1a6ffbab79e68f8a23c118f4723
Nicolas <nicolas.gonzalez.alexi@gmail.com>
2021-11-17 15:47:43 -0300

    inspección de web y url correcta

--- project.py
+++ project.py
@@ -6,3 +6,20 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import requests
 import time
+
+class AddRemoveElements(unittest.TestCase):
+	def setUp(self):
+		self.datos = urllib.request.urlopen('http://books.toscrape.com/').read().decode()
+
+	def test_search_ps4(self):
+		datos = self.datos
+		soup =  BeautifulSoup(datos)
+		tags = soup('a')
+		urll = []
+		for tag in tags:
+			urll.append(tag.get('href'))
+		df = pd.DataFrame({'url': urll})
+		df.to_csv('libros_url.csv')
+
+if __name__ == "__main__":
+    unittest.main(verbosity = 2)
