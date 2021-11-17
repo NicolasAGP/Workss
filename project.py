5838f286e2455b6b21b232258432bd891847aaaa
Nicolas <nicolas.gonzalez.alexi@gmail.com>
2021-11-17 16:29:58 -0300

    falla, en codigo

--- project.py
+++ project.py
@@ -7,19 +7,25 @@ import pandas as pd
 import requests
 import time
 
+URL_BASE = 'http://books.toscrape.com/'
+urll = []
 class AddRemoveElements(unittest.TestCase):
 	def setUp(self):
 		self.datos = urllib.request.urlopen('http://books.toscrape.com/').read().decode()
 
 	def test_search_ps4(self):
-		datos = self.datos
-		soup =  BeautifulSoup(datos)
-		tags = soup('a')
-		urll = []
-		for tag in tags:
-			urll.append(tag.get('href'))
-		df = pd.DataFrame({'url': urll})
-		df.to_csv('libros_url.csv')
+		for i in range(1, 10):
+
+			if i > 1:
+				dats = "%s/catalogue/page-%d.html" % (self.datos, i)
+				r = requests.get(dats)
+            	soup =  BeautifulSoup(r.text, 'html.parser')					
+			else:
+				print('pass')
+	def ss(self):
+				
+
+		
 
 if __name__ == "__main__":
-    unittest.main(verbosity = 2)
+   	unittest.main(verbosity = 2)
